e2148f703faaa7b23f56b287a9650775e61554e9
robertmacyiii <robert.macyiii@gmail.com>
2016-12-31 13:42:58 -0800

    Updated reasoning io

--- service_v2-0.py
+++ service_v2-0.py
@@ -14,23 +14,26 @@ def process_runs(run_queue):
     return processing.process_runs_sequential(run_queue, process_run)
 
 def process_run(id, input, log):
-    import run_bn_inference_v2_0 as reasoning_system
+    import generate_Medifor_bn_model_9 as reasoning_system
+    #The line below training the model can be omitted if the base bayesian network file is already in the working directory.
+    #reasoning_system.train_model()
     
     # Input should consist of algorithms names, scores, and heat maps
     # NOTE: Hardcoded naming scheme.  A bit hackish.
     # TODO -- Does this need better error correction?  What if score or
     # heatmap is missing?
     log.info('Collecting algorithm scores and heatmaps')
-    algs = input['algorithms'].split(',')
-    scores   = {a: input[a + '_score'] for a in algs}
-    heatmaps = {a: deserialize_image(input[a + '_heatmap'].data) for a in algs}
+    #algs = input['algorithms'].split(',')
+    #scores   = {a: input[a + '_score'] for a in algs}
+    #heatmaps = {a: deserialize_image(input[a + '_heatmap'].data) for a in algs}
 
     log.info('Running probabilistic inference')
 
     ###############################
     # TODO -- run inference here.
     ###############################
-    manips, confidences, heatmaps = reasoning_system.run_inference(scores, heatmaps)
+    results = reasoning_system.run_inference(input)
+    #manips, confidences, heatmaps = reasoning_system.run_inference(scores, heatmaps)
 
     # STUB -- replace this with something like the commented out line above.
     #manips, confidences, heatmaps = [], [], []
@@ -39,16 +42,16 @@ def process_run(id, input, log):
     # Generate output dictionary
     #
     log.info('Collecting and returning inference results')
-    output = {}
+    output = results
 
     # List of all manipulations that we're making predictions for
-    output['manipulations'] = ",".join(manips)
+    #output['manipulations'] = ",".join(manips)
 
     # Set confidence and heatmap for each manipulation
     # (Heatmap value could be "None" if unavailable.)
-    for m, c, h in zip(manips, confidences, heatmaps):
-        output[m + '_confidence'] = h
-        output[m + '_heatmap'] = c
+    #for m, c, h in zip(manips, confidences, heatmaps):
+    #    output[m + '_confidence'] = h
+    #    output[m + '_heatmap'] = c
 
     return output
 
@@ -70,29 +73,19 @@ if __name__ == '__main__':
 
     # TODO -- Create better test input...
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(message)s')
-    heatmap1 = fileutil.read_binary('../../../../exp_pt1/block02/mask/NC2016_0016.png')
-    heatmap2 = fileutil.read_binary('../../../../exp_pt1/dct01/mask/NC2016_0016.png')
+    #heatmap1 = fileutil.read_binary('../../../../exp_pt1/block02/mask/NC2016_0016.png')dd
+    heatmap1 = imread('NC2016_0016_block02.png')
+    #heatmap2 = fileutil.read_binary('../../../../exp_pt1/dct01/mask/NC2016_0016.png')
+    heatmap2 = imread('NC2016_0016_dct01.png')
+    query_image = imread('NC2016_0016.jpg')
     id = uuid4()
     input = {
-        'algorithms': 'block01,block02,copymove01,dct01,dct02,dct03_A,dct03_NA,ela01,noise01,noise02',
-        'block01_score': 'nan',
-        'block01_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'block02_score': 12.34,
-        'block02_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'dct01_score':  0.0,
-        'dct01_heatmap': Resource('image', heatmap2, 'image/jpeg'),
-        'dct02_score':  0.0,
-        'dct02_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'dct03_A_score':  0.0,
-        'dct03_A_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'dct03_NA_score':  0.0,
-        'dct03_NA_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'ela01_score':  0.0,
-        'ela01_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'noise01_score':  0.0,
-        'noise01_heatmap': Resource('image', heatmap1, 'image/jpeg'),
-        'noise02_score':  0.0,
-        'noise02_heatmap': Resource('image', heatmap1, 'image/jpeg')
+        # The input format requires the query_image, a list of relevant algorithms (must be complete), and any evidence the user has regarding the algorithms (i.e. scores and/or heatmaps).
+        'image': Resource('image', query_image, 'image/jpeg'),
+        'algorithms': ['block01', 'block02', 'copymove01', 'dct01', 'dct02', 'dct03_A', 'dct03_NA', 'ela01', 'noise01', 'noise02'],
+        'block02': {'heatmap': Resource('image', heatmap1, 'image/png')},
+        'dct01': {'score': '12.04', 'heatmap': Resource('image', heatmap2, 'image/png')}
     }
     log = logging.getLogger(__name__)
-    process_run(id, input, log)
+    output = process_run(id, input, log)
+    print(output)
